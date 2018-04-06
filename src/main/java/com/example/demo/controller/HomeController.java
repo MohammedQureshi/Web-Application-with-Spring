276f77e8519338399d634154ce38bad0ca79ba40
James Millner <u1257434@unimail.hud.ac.uk>
2018-04-06 16:09:16 +0100

    Core MVC working.
    M- Models
    V- Views
    C- Controllers

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,11 +5,12 @@ import com.example.demo.domain.HelloPOJORepository;
 import com.example.demo.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -23,13 +24,34 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String helloWorld(Map<String, Object> model) {
 
+        //Java 8 Version
+
+        model.put("pojo", new HelloPOJO("James", "Hello Again!!"));
+        model.put("pojo2", new HelloPOJO("James2", "Hello Again!asdasdasdasd!"));
+        model.put("pojo3", new HelloPOJO("James", "Hello Again I'm number 3!!"));
+
+        HelloPOJO p = (HelloPOJO) model.get("pojo");
+
         return "homepage";
     }
 
-    @RequestMapping(value = "/about", method = RequestMethod.GET)
-    public String about() {
 
-        return "about-me";
+
+    /**
+     * Typical API response method.
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/api", method = RequestMethod.GET)
+    public List<HelloPOJO> getHelloPojos() {
+
+        Map<String, HelloPOJO> model = new HashMap<>();
+
+        model.put("pojo", new HelloPOJO("James", "Hello Again!!"));
+        model.put("pojo2", new HelloPOJO("James2", "Hello Again!asdasdasdasd!"));
+        model.put("pojo3", new HelloPOJO("James", "Hello Again I'm number 3!!"));
+
+        return new ArrayList<>(model.values());
     }
 
 }
