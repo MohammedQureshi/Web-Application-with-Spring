8c630f97bf213804d418c3507a01c1e9a868b2f1
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2018-07-05 12:21:48 +0100

    Load Record
    Loading player records by checking for first name of the player.

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.domain.HelloPOJO;
 import com.example.demo.domain.HelloPOJORepository;
 import com.example.demo.domain.playerHolder;
 import com.example.demo.domain.playerHolderRepo;
@@ -10,9 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Map;
-import java.util.UUID;
 
 @Controller
 public class HomeController {
@@ -26,6 +23,10 @@ public class HomeController {
     @Autowired
     private HelloPOJORepository repository;
 
+
+    /*
+    Homepage will contain all the navigation buttons.
+     */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String helloWorld(Map<String, Object> model) {
 
@@ -36,16 +37,26 @@ public class HomeController {
     This page just loads up the web page that contains the form to insert the information into.
      */
     @RequestMapping(value = "/createRecord", method = RequestMethod.GET)
-    public String loadRecord(Map<String, Object> model) {
+    public String createRecord(Map<String, Object> model) {
 
         return "createRecord";
     }
 
+
+    /*
+    This method loads the player record and stores the information.
+     */
+    @RequestMapping(value = "/loadRecord", method = RequestMethod.GET)
+    public String loadRecord(@RequestParam("firstName") String fName) {
+        playerHolder storedPlayer = playerRepo.findByName(fName);
+        System.out.println(storedPlayer.getLastName());
+        return "loadRecord";
+    }
+
     /*
     Method before collects the information from the web form and then stores it within the MySQL database to be accessed later and then will direct you back to
-    the homepage to access other areas of the web page such as going back to the start menu. acceptable
+    the homepage to access other areas of the web page such as going back to the start menu. acceptable.
      */
-
     @RequestMapping(value = "/saveRecord", method = RequestMethod.GET)
     public String playerInfo(@RequestParam("firstName") String fName, @RequestParam("lastName") String lName, @RequestParam("wins") int wins, @RequestParam("loses") int loses, @RequestParam("draws") int draws,@RequestParam("contactNum") String telNum ){
         playerHolder player = new playerHolder(fName, lName, wins, loses, draws, telNum);
